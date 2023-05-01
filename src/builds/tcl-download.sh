6cf0d8fafe7e4f6d766eb3910484e58ffebad4bd
alt-s6-builder-s <>
2023-05-01 11:41:10 +0000

    updated else code
    FossilOrigin-Name: 1aafe2ada243b0dd5061cc245505da81a05a5d002ca3f47987e2ad8155f348f5

--- src/builds/tcl-download.sh
+++ src/builds/tcl-download.sh
@@ -3,5 +3,5 @@ if [ ! -f tcl"${TCL_VERSION}"-src.tar.gz ] ; then
     echo wget "${WGET_OPTIONS}" https://downloads.sourceforge.net/sourceforge/tcl/tcl"${TCL_VERSION}"-src.tar.gz 2>&1
     wget "${WGET_OPTIONS}" https://downloads.sourceforge.net/sourceforge/tcl/tcl"${TCL_VERSION}"-src.tar.gz 2>&1
 else
-    echo "File tcl"${TCL_VERSION}"-src.tar.gz exist. Skip download." 2>&1
+    echo "File tcl${TCL_VERSION}-src.tar.gz exist. Skip download." 2>&1
 fi
--- src/builds/tcllib-download.sh
+++ src/builds/tcllib-download.sh
@@ -2,5 +2,5 @@
 if [ ! -f tcllib-"${TCLLIB_VERSION}".tar.bz2 ] ; then
     wget "${WGET_OPTIONS}" https://downloads.sourceforge.net/sourceforge/tcllib/tcllib-"${TCLLIB_VERSION}".tar.bz2
 else
-    echo "tcllib-"${TCLLIB_VERSION}".tar.bz2 exist. Skip download." 2>&1
+    echo "tcllib-${TCLLIB_VERSION}.tar.bz2 exist. Skip download." 2>&1
 fi
